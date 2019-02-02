0db3dc5918ba935ca9f6b0ce66fc3a0100e475d4
Darci Neto <darci.neto@hotmail.com>
2019-02-02 17:19:23 -0200

    Classes do bootstrap no html

--- resources/views/listagem.php
+++ resources/views/listagem.php
@@ -1,16 +1,22 @@
 <html>
+    <head>
+        <link href="/css/app.css" rel="stylesheet">
+        <title>Controle de estoque</title>
+    </head>
     <body>
-        <h1>Listagem de produtos</h1>
-        <table>
-            <?php foreach ($produtos as $p): ?>
-            <tr>
-            <td><?php echo $p->nome ?> </td>
-            <td><?php echo $p->valor ?> </td>
-            <td><?php echo $p->descricao ?> </td>
-            <td><?php echo $p->quantidade ?> </td>
-            </tr>
-            <?php endforeach ?>
-        </table>
+        <div class="container">
+            <h1>Listagem de produtos</h1>
+            <table class="table table-striped table-bordered table-hover">
+                <?php foreach ($produtos as $p): ?>
+                <tr>
+                <td><?php echo $p->nome ?> </td>
+                <td><?php echo $p->valor ?> </td>
+                <td><?php echo $p->descricao ?> </td>
+                <td><?php echo $p->quantidade ?> </td>
+                </tr>
+                <?php endforeach ?>
+            </table>
+        </div>
     </body>
 </html>
 
